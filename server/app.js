87f02eba958286a613b35b9003f89f6732a9d12e
ajayvisu <ajay.platosys@gmail.com>
2022-10-18 17:31:28 +0530

    home page UI enhanced

--- server/app.js
+++ server/app.js
@@ -3,9 +3,20 @@ const app = express();
 const mongoose = require ("mongoose");
 require('dotenv').config();
 const database = require ('./database/db')
+const employee = require('./routes/emp.route');
+const cors = require('cors');
+const PORT = process.env.PORT;
+
+app.use(express.json())
+app.use(cors());
+app.use('/api/emp',employee);
+
+app.get('/',(req,res)=>{
+    res.send({status:'sucuss',message:'its working'})
+})
 
 
 
 app.listen(PORT, () =>{
-    console.log("Server created")
+    console.log(`Server created ${PORT}`)
 })
