4ac7b26bb93894117f999e8a22656777d7563148
ajayvisu <ajay.platosys@gmail.com>
2022-10-19 11:21:13 +0530

    created routes

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -6,19 +6,31 @@ const Home = () => {
     <>
       <div className="container mt-3">
         <div className="row">
-        <div className="col-lg-4 col-sm-6 col-6 offset-lg-4 offset-sm-3 offset-3 ">
+          <div className="col-lg-4 col-sm-6 col-6 offset-lg-4 offset-sm-3 offset-3 ">
             <div className="row gy-3">
               <div className="col-md-6 col-12">
-                <button className="btn firstbtn1 w-100 py-5">HR</button>
+                <a href="/hr">
+                  <button className="btn firstbtn1 w-100 py-5">HR</button>
+                </a>
               </div>
               <div className="col-md-6 col-12">
-                <button className="btn firstbtn2 w-100 py-5">EMPLOYEE</button>
+                <a href="/emp">
+                  <button className="btn firstbtn2 w-100 py-5">EMPLOYEE</button>
+                </a>
               </div>
               <div className="col-md-6 col-12">
-                <button className="btn firstbtn1 w-100 py-5">MANAGEMENT</button>
+                <a href="/mgmt">
+                  <button className="btn firstbtn1 w-100 py-5">
+                    MANAGEMENT
+                  </button>
+                </a>
               </div>
               <div className="col-md-6 col-12">
-                <button className="btn firstbtn2 w-100 py-5">MAINTANANCE</button>
+                <a href="/maint">
+                  <button className="btn firstbtn2 w-100 py-5">
+                    MAINTANANCE
+                  </button>
+                </a>
               </div>
             </div>
           </div>
